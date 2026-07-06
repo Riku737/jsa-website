ce47655a41405062edd59a86de8c8aa009c778d3
Riki <rmcal024@uottawa.ca>
2026-07-06 14:02:39 -0400

    Navbar hover fix

--- jsa/main/urls.py
+++ jsa/main/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("home", views.home),
-    path("archive", views.archive),
-    path("calendar", views.calendar),
-    path("jcg", views.jcg),
-    path("team", views.team),
+    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("archive", views.archive, name="archive"),
+    path("calendar", views.calendar, name="calendar"),
+    path("jcg", views.jcg, name="jcg"),
+    path("team", views.team, name="team"),
 ]
